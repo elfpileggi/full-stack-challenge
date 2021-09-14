392ed3a5716ee1d2fbb5d12989434c29ac48f2a4
Fernando Pileggi <fernando.pileggi@ahgora.com.br>
2021-09-14 10:58:29 -0300

    add getAll and getById queries on student repository

--- api/src/data/repositories/student-repository.js
+++ api/src/data/repositories/student-repository.js
@@ -10,4 +10,15 @@ module.exports = {
 
     return result
   },
+  getById: async (id) => {
+    if (!id && id !== 0) throw new MissingParamError('ID')
+    const list = model.getBy({ id })
+    const result = list.length > 0 ? list[0] : null
+
+    return result
+  },
+  getAll: async () => {
+    const list = model.getBy({})
+    return list
+  }
 }
--- api/src/data/repositories/student-repository.spec.js
+++ api/src/data/repositories/student-repository.spec.js
@@ -50,4 +50,47 @@ describe('Student Repository', () => {
     
   })
 
+  
+  describe('Calling getById', () => {
+    test('Should throw if required parameters are not provided', async () => {
+      const promise = sut.getById()
+      await expect(promise).rejects.toThrow()
+    })
+    
+    test('Should throw if id is not provided', async () => {
+      const promise = sut.getById()
+      await expect(promise).rejects.toThrow(new MissingParamError('ID'))
+    })
+
+    test('Should return object if required parameters are provided', async () => {
+      const expected = {
+        id: 1,
+        name: "Tester",
+        email: "test@soraschools.com",
+        house: "Heqet",
+        skills: [
+          { id: 3, level: 2 },
+          { id: 4, level: 3 },
+          { id: 6, level: 1 },
+        ],
+      }
+      const result = await sut.getById(1)
+      expect(typeof result).toEqual('object')
+      expect(result).toEqual(expected)
+    })
+
+    test('Should return null if required parameters are provided but no results', async () => {
+      const result = await sut.getById(0)
+      expect(result).toBeNull()
+    })
+  })
+  
+  describe('Calling getAll', () => {
+    test('Should return array if required parameters are provided', async () => {
+      const result = await sut.getAll()
+      expect(result.length).toBeGreaterThan(0)
+    })
+  })
+  
+
 })
