9360e283630481e3ad4c0f7c11203bbe00e6e118
Fernando Pileggi <fernando.pileggi@ahgora.com.br>
2021-09-14 00:37:07 -0300

    add level assert to student repository

--- api/src/data/repositories/student-repository.js
+++ api/src/data/repositories/student-repository.js
@@ -2,9 +2,10 @@ const { MissingParamError } = require("../../utils/errors")
 const model = require("../models/student-model")
 
 module.exports = {
-  getBySkillId: async (skillId) => {
+  getBySkillIdAndGreaterLevel: async (skillId, level) => {
     if (!skillId && skillId !== 0) throw new MissingParamError('Skill ID')
-    const list = model.getBy({ skills: [{ id: skillId }] })
+    if (!level && level !== 0) throw new MissingParamError('Level')
+    const list = model.getBy({ skills: [{ id: skillId, level: `>${level}` }] })
     const result = list.length > 0 ? list[0] : null
 
     return result
--- api/src/data/repositories/student-repository.spec.js
+++ api/src/data/repositories/student-repository.spec.js
@@ -3,16 +3,21 @@ const sut = require('./student-repository')
 
 describe('Student Repository', () => {
     
-  describe('Calling getBySkillId', () => {
+  describe('Calling getBySkillIdAndGreaterLevel', () => {
 
     test('Should throw if required parameters are not provided', async () => {
-      const promise = sut.getBySkillId()
+      const promise = sut.getBySkillIdAndGreaterLevel()
       await expect(promise).rejects.toThrow()
     })
     
     test('Should throw if id is not provided', async () => {
-      const promise = sut.getBySkillId()
+      const promise = sut.getBySkillIdAndGreaterLevel()
       await expect(promise).rejects.toThrow(new MissingParamError('Skill ID'))
+    })
+
+    test('Should throw if id is not provided', async () => {
+      const promise = sut.getBySkillIdAndGreaterLevel(1)
+      await expect(promise).rejects.toThrow(new MissingParamError('Level'))
     })
 
     test('Should return object if required parameters are provided', async () => {
@@ -27,13 +32,19 @@ describe('Student Repository', () => {
           { id: 6, level: 1 },
         ],
       }
-      const result = await sut.getBySkillId(3)
+      const result = await sut.getBySkillIdAndGreaterLevel(3, 1)
       expect(typeof result).toEqual('object')
       expect(result).toEqual(expected)
     })
 
+    test('Should return null if required parameters are provided but with higher level', async () => {
+      const result = await sut.getBySkillIdAndGreaterLevel(3, 4)
+      expect(typeof result).toEqual('object')
+      expect(result).toBeNull()
+    })
+
     test('Should return null if required parameters are provided but no results', async () => {
-      const result = await sut.getBySkillId(0)
+      const result = await sut.getBySkillIdAndGreaterLevel(0, 0)
       expect(result).toBeNull()
     })
     
