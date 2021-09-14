7dca51d41fde73cd879cd6a6000f673d5caa4d96
Fernando Pileggi <fernando.pileggi@ahgora.com.br>
2021-09-14 12:22:03 -0300

    add getById to faculty repository with tests

--- api/src/data/repositories/faculty-repository.js
+++ api/src/data/repositories/faculty-repository.js
@@ -2,11 +2,18 @@ const { MissingParamError } = require("../../utils/errors")
 const model = require("../models/faculty-model")
 
 module.exports = {
+  getById: async (id) => {
+    if (!id && id !== 0) throw new MissingParamError('ID')
+    const list = model.getBy({ id })
+    const result = list.length > 0 ? list[0] : null
+
+    return result
+  },
   getBySubjectId: async (subjectId) => {
     if (!subjectId && subjectId !== 0) throw new MissingParamError('Subject ID')
     const list = model.getBy({ subject_ids: [subjectId] })
     const result = list.length > 0 ? list[0] : null
 
     return result
-  },
+  }
 }
--- api/src/data/repositories/faculty-repository.spec.js
+++ api/src/data/repositories/faculty-repository.spec.js
@@ -2,6 +2,34 @@ const { MissingParamError } = require('../../utils/errors')
 const sut = require('./faculty-repository')
 
 describe('Faculty Repository', () => {
+
+  describe('Calling getById', () => {
+    test('Should throw if required parameters are not provided', async () => {
+      const promise = sut.getById()
+      await expect(promise).rejects.toThrow()
+    })
+    
+    test('Should throw if id is not provided', async () => {
+      const promise = sut.getById()
+      await expect(promise).rejects.toThrow(new MissingParamError('ID'))
+    })
+
+    test('Should return object if required parameters are provided', async () => {
+      const expected = {
+        id: 1,
+        name: 'Michael Granado',
+        subject_ids: [3, 4]
+      }
+      const result = await sut.getById(1)
+      expect(typeof result).toEqual('object')
+      expect(result).toEqual(expected)
+    })
+
+    test('Should return null if required parameters are provided but no results', async () => {
+      const result = await sut.getById(0)
+      expect(result).toBeNull()
+    })
+  })
     
   describe('Calling getBySubjectId', () => {
 
